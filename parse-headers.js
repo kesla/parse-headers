13e8e72087e9568c437f5791965d975c44761707
yassh <yassh32@outlook.jp>
2018-07-15 09:39:01 +0900

    uninstall for-each

--- parse-headers.js
+++ parse-headers.js
@@ -1,5 +1,4 @@
-var forEach = require('for-each')
-  , isArray = function(arg) {
+var isArray = function(arg) {
       return Object.prototype.toString.call(arg) === '[object Array]';
     }
 
@@ -9,22 +8,19 @@ module.exports = function (headers) {
 
   var result = {}
 
-  forEach(
-      headers.trim().split('\n')
-    , function (row) {
-        var index = row.indexOf(':')
-          , key = row.slice(0, index).trim().toLowerCase()
-          , value = row.slice(index + 1).trim()
+  headers.trim().split('\n').forEach(function (row) {
+    var index = row.indexOf(':')
+      , key = row.slice(0, index).trim().toLowerCase()
+      , value = row.slice(index + 1).trim()
 
-        if (typeof(result[key]) === 'undefined') {
-          result[key] = value
-        } else if (isArray(result[key])) {
-          result[key].push(value)
-        } else {
-          result[key] = [ result[key], value ]
-        }
-      }
-  )
+    if (typeof(result[key]) === 'undefined') {
+      result[key] = value
+    } else if (isArray(result[key])) {
+      result[key].push(value)
+    } else {
+      result[key] = [ result[key], value ]
+    }
+  })
 
   return result
 }
